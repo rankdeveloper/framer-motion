3173012e86409763c0e3bea42d326b45fbf0c6f6
Rankush <iamrankush@gmail.com>
2024-10-24 23:56:36 +0530

    text , design change

--- src/LayoutGroup.tsx
+++ src/LayoutGroup.tsx
@@ -3,9 +3,21 @@ import React, { useState } from "react";
 import { motion, LayoutGroup } from "framer-motion"; // Optional for styling
 
 const items = [
-  { id: 1, title: "Item 1", description: "This is the description for item 1" },
-  { id: 2, title: "Item 2", description: "This is the description for item 2" },
-  { id: 3, title: "Item 3", description: "This is the description for item 3" },
+  {
+    id: 1,
+    title: "JavaScript",
+    description: "JavaScript is a programming language",
+  },
+  {
+    id: 2,
+    title: "TypeScript",
+    description: "TypeScript is a superset of JavaScript",
+  },
+  {
+    id: 3,
+    title: "Framer Motion",
+    description: "Framer Motion is a motion library",
+  },
 ];
 
 const LayoutGroupExample1 = () => {
@@ -25,7 +37,7 @@ const LayoutGroupExample1 = () => {
         <motion.div
           key={item.id}
           layout
-          className="bg-blue-400  py-2 px-5 rounded-md shadow-[0_4px_6px_rgba(0,0,0,0.1)] overflow-hidden cursor-pointer"
+          className="bg-blue-400  py-2 px-5 rounded-md shadow-[0_4px_6px_rgba(0,0,0,0.1)] overflow-hidden cursor-pointer min-w-[200px] mb-2 "
           whileHover={{ scale: 1.03 }}
           onClick={() => expand(item.id)}
         >
